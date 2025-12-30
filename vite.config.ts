4e00f6931e58ee050894765d57eb61143cfac578
valorix-marketing <valorix.marketing.agency@gmail.com>
2025-12-30 18:36:24 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: `/${joker-gym-builder}/`
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
